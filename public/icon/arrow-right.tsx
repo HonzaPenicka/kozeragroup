f44100867f3f34141e214b18db8368bc272adc3a
Jan Pěnička <jan.penicka@ceskatelevize.cz>
2024-12-01 01:59:45 +0100

    first try od design

---
+++ public/icon/arrow-right.tsx
@@ -0,0 +1,21 @@
+import { FunctionComponent } from "react";
+
+export const ArrowRightIcon: FunctionComponent = () => {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M5 12H19M19 12L13 6M19 12L13 18"
+        stroke="#000000"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,114 +1,143 @@
 import Image from "next/image";
 import localFont from "next/font/local";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+import { Swiper, SwiperSlide } from "swiper/react";
+import { ArrowRightIcon } from "../../public/icon/arrow-right";
+import { XIcon } from "../../public/icon/x";
+import { InstagramIcon } from "../../public/icon/instagram";
+import { FacebookIcon } from "../../public/icon/facebook";
 
 export default function Home() {
   return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="mx-auto container p-8 text-black text-lg">
+      <header className="pb-8 uppercase font-bold">
+        <div>Kozera Group</div>
+      </header>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+      <main className="flex flex-col gap-2 md:gap-8 lg:gap-16">
+        <div className="aspect-square md:aspect-video bg-pink-400 items-center grid p-4 md:p-8 lg:p-16 gap-8 py-16 rounded-xl">
+          <h1 className="text-2xl md:text-4xl lg:text-6xl font-bold">
+            Own Your Luxury Dream Estate
+          </h1>
+
+          <h2 className="text-pretty lg:text-justify text-sm md:w-3/5">
+            Naflušu ti do držky sračky, lízej mi prdel. Sexistické narážku na
+            mojí obří kundu? Zešvihám ti prdel ty píčo! Natrhnu ti kundu děfko!
+          </h2>
+
+          <div className="flex bg-yellow-600 gap-4 lg:gap-8">
+            <XIcon />
+
+            <InstagramIcon />
+
+            <FacebookIcon />
+          </div>
+
+          <div>
+            <button className="flex items-center gap-2 lg:gap-4 bg-green-400 py-2 lg:py-8 px-4 lg:px-16 rounded-xl font-semibold text-md">
+              <div>Learn More</div>
+
+              <ArrowRightIcon />
+            </button>
+          </div>
+        </div>
+
+        <div className="grid md:grid-cols-3 gap-2 md:gap-8 lg:gap-16 pb-4">
+          <div className="apect-video bg-blue-500 py-8 px-4 lg:px-16 lg:py-16 rounded-xl grid gap-2 lg:gap-4">
+            <div className="text-2xl font-semibold">1200+</div>
+
+            <div className="text-sm">
+              Companies affiliate <br />
+              with us
+            </div>
+          </div>
+
+          <div className="apect-video bg-blue-500 py-8 px-4 lg:px-16 lg:py-16 rounded-xl grid gap-2 lg:gap-4">
+            <div className="text-2xl font-semibold">8M+</div>
+
+            <div className="text-sm">
+              Happy customers
+              <br />
+              <br />
+            </div>
+          </div>
+
+          <div className="apect-video bg-blue-500 py-8 px-4 lg:px-16 lg:py-16 rounded-xl grid gap-2 lg:gap-4">
+            <div className="text-2xl font-semibold">9+</div>
+
+            <div className="text-sm">
+              Years of service
+              <br />
+              <br />
+            </div>
+          </div>
+        </div>
+
+        <div className="grid py-4 bg-slate-300 rounded-xl">
+          <div className="flex flex-col md:flex-row md:justify-between gap-4 p-4 md:p-8 lg:p-16">
+            <div className="flex flex-col gap-4 lg:gap-8 md:w-2/5">
+              <div className="text-2xl lg:text-4xl font-semibold">
+                Our Partnerships
+              </div>
+
+              <div className="text-sm">
+                Ceckatý Čůráček, Chlupatý Píčípískoviště,
+                <br /> Kokotí Rolky
+              </div>
+            </div>
+
+            <div className="grid grid-cols-2 md:grid-cols-3 col-span-2 gap-4 lg:gap-8">
+              <div className="p-4 lg:py-8 lg:px-24 bg-green-400 rounded-xl flex justify-center">
+                Leopard
+              </div>
+
+              <div className="p-4 lg:py-8 lg:px-24 bg-red-600 rounded-xl flex justify-center">
+                Leopard
+              </div>
+
+              <div className="p-4 lg:py-8 lg:px-16 bg-orange-500 rounded-xl flex justify-center">
+                Leopard
+              </div>
+
+              <div className="p-4 lg:py-8 lg:px-16 bg-green-400 rounded-xl flex justify-center">
+                Leopard
+              </div>
+
+              <div className="p-4 lg:py-8 lg:px-16 bg-red-600 rounded-xl flex justify-center">
+                Leopard
+              </div>
+
+              <div className="p-4 lg:py-8 lg:px-16 bg-orange-500 rounded-xl flex justify-center">
+                Leopard
+              </div>
+            </div>
+          </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+
+      <footer className="px-4 md:px-8 pt-8 grid grid-cols-2 gap-4 text-sm">
+        <div className="text-left grid gap-2">
+          <XIcon />
+
+          <div className="font-semibold">E-mailová adresa</div>
+
+          <div className="text-gray-500">kozera@group.cz</div>
+        </div>
+
+        <div className="text-left grid gap-2">
+          <XIcon />
+
+          <div className="font-semibold">Telefonní číslo</div>
+
+          <div className="text-gray-500">+420 608 666 200</div>
+        </div>
+
+        <div className="text-left grid gap-2">
+          <XIcon />
+
+          <div className="font-semibold">Adresa</div>
+
+          <div className="text-gray-500">U Omrdaný Kundy 01 MRD, Ceckovice</div>
+        </div>
       </footer>
     </div>
   );
