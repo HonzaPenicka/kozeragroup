e9a810736f890581c67c55d3542278a564e950a2
Jan Pěnička <jan.penicka@ceskatelevize.cz>
2024-12-03 21:01:52 +0100

    analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,11 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <Analytics />
         <title>Kozera Group - Správa Investic a Rozvoj</title>
         <meta
           name="description"
